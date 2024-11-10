d54ecf4e0fe2e8a1312d33121e9227e03b42359e
mohamedshaban <sarahmama@moe.edu.kw>
2024-11-10 22:19:09 +0300

    task2 solved

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import store from './store.jpg'
 import './App.css';
 
+
+import products from './Products'
 function App() {
   return (
     <div className="App">
 
        <h1>Sarah's Store</h1>
    <p> make your own Hoodies and tote bag</p>
-  <img src={store} alt='store image' width="500px"/>
+  <img src={store} alt='store image' width="100%" height="400px"/>
+  <div className='products'>
+  {products.map((product) =>(
+    <div >
+      <img src={product.image} width="200px"/>
+      <h2>{product.name}</h2>
+      <h4>{product.price}</h4>
+      </div>)
+     )
+     }
+
+  </div>
     </div>
   );
 }
---
+++ src/Products.js
@@ -0,0 +1,14 @@
+
+import product1 from './product1.jpg'
+import product2 from './product2.jpg'
+
+
+const products=[
+    {
+        image:product1,name:"Product1", price:"8.00 KD"
+    },     {
+        image:product2,name:"Product2", price:"5.00 KD"
+    }
+];
+
+  export default products;
