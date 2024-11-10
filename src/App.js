41fbd1a6f4ff9ab3af72f6a101864fb4413def7f
mohamedshaban <sarahmama@moe.edu.kw>
2024-11-10 19:31:59 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import store from './store.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+       <h1>Sarah's Store</h1>
+   <p> make your own Hoodies and tote bag</p>
+  <img src={store} alt='store image' width="500px"/>
+    </div>
+  );
+}
+
+export default App;
