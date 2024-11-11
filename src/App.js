74b154ea5187fd86dab816f53c54fb6e0564938a
mohamedshaban <sarahmama@moe.edu.kw>
2024-11-11 18:58:57 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import store from './store.jpg'
 import './App.css';
-import products from './Products'
+import products from './products'
 
 function App() {
-  const productList =   products.map((product,index) =>{return(
-    <div key={index}>
-      <img src={product.image} width="200px"/>
-      <h2>{product.name}</h2>
-      <h4>{product.price}</h4>
-      </div>)}
-     )
-     
+  const productList = products.map((product, index) => {
+
+      <div key={index}>
+        <img src={product.image} width="200px" />
+        <h2>{product.name}</h2>
+        <h4>{product.price}</h4>
+      </div>
+  }
+  );
+
   return (
     <div className="App">
 
-       <h1>Sarah's Store</h1>
-   <p> make your own Hoodies and tote bag</p>
-  <img src={store} alt='store image' width="100%" height="400px"/>
-  <div className='products'>
-{productList}
+      <h1>Sarah's Store</h1>
+      <p> make your own Hoodies and tote bag</p>
+      <img src={store} alt='store image' width="100%" height="400px" />
+      <div className='products'>
+        {productList}
 
-  </div>
+      </div>
     </div>
   );
 }
