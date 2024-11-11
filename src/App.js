b1d47fab4d4810973b7e63656f931c01641b4f30
mohamedshaban <sarahmama@moe.edu.kw>
2024-11-11 10:15:03 +0300

    task3 solved

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import store from './store.jpg'
 import './App.css';
-
-
 import products from './Products'
+
 function App() {
+  const productList =   products.map((product) =>(
+    <div >
+      <img src={product.image} width="200px"/>
+      <h2>{product.name}</h2>
+      <h4>{product.price}</h4>
+      </div>)
+     )
+     
   return (
     <div className="App">
 
@@ -11,14 +18,7 @@ function App() {
    <p> make your own Hoodies and tote bag</p>
   <img src={store} alt='store image' width="100%" height="400px"/>
   <div className='products'>
-  {products.map((product) =>(
-    <div >
-      <img src={product.image} width="200px"/>
-      <h2>{product.name}</h2>
-      <h4>{product.price}</h4>
-      </div>)
-     )
-     }
+{productList}
 
   </div>
     </div>
